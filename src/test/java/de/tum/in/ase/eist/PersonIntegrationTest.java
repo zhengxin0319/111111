helenjim95/Containers

Exercise-Template pushed by Artemis

src/test/java/de/tum/in/ase/eist/PersonIntegrationTest.java
@@ -1,0 +1,75 @@
+package de.tum.in.ase.eist;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.tum.in.ase.eist.model.Person;
+import de.tum.in.ase.eist.repository.PersonRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+class PersonIntegrationTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    private static ObjectMapper objectMapper;
+
+    @BeforeAll
+    static void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+    }
+
+    @Test
+    void testAddPerson() throws Exception {
+        var person = new Person();
+        person.setFirstName("Max");
+        person.setLastName("Mustermann");
+        person.setBirthday(LocalDate.now());
+
+        var response = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(person))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(1, personRepository.findAll().size());
+    }
+
+    @Test
+    void testDeletePerson() throws Exception {
+        var person = new Person();
+        person.setFirstName("Max");
+        person.setLastName("Mustermann");
+        person.setBirthday(LocalDate.now());
+
+        person = personRepository.save(person);
+
+        var response = this.mvc.perform(
+                delete("/persons/" + person.getId())
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatus());
+        assertTrue(personRepository.findAll().isEmpty());
+    }
+}

src/test/java/de/tum/in/ase/eist/PersonServiceTest.java
@@ -1,0 +1,53 @@
+package de.tum.in.ase.eist;
+
+import de.tum.in.ase.eist.model.Person;
+import de.tum.in.ase.eist.repository.PersonRepository;
+import de.tum.in.ase.eist.service.PersonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+class PersonServiceTest {
+    @Autowired
+    private PersonService personService;
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void testAddPerson() {
+        var person = new Person();
+        person.setFirstName("Max");
+        person.setLastName("Mustermann");
+        person.setBirthday(LocalDate.now());
+
+        personService.save(person);
+
+        assertEquals(1, personRepository.findAll().size());
+    }
+
+    @Test
+    void testDeletePerson() {
+        var person = new Person();
+        person.setFirstName("Max");
+        person.setLastName("Mustermann");
+        person.setBirthday(LocalDate.now());
+
+        person = personRepository.save(person);
+
+        personService.delete(person);
+
+        assertTrue(personRepository.findAll().isEmpty());
+    }
+}

zhengxin0319/111111

src/test/java/de/tum/in/ase/eist/PersonIntegrationTest.java
@@ -1,0 +1,77 @@
+package de.tum.in.ase.eist;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.tum.in.ase.eist.model.Person;
+import de.tum.in.ase.eist.repository.PersonRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+class PersonIntegrationTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    private static ObjectMapper objectMapper;
+
+    @BeforeAll
+    static void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+    }
+
+    @Test
+    void testAddPerson() throws Exception {
+        var person = new Person();
+        person.setFirstName("Max");
+        person.setLastName("Mustermann");
+        person.setBirthday(LocalDate.now());
+
+        var response = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(person))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(1, personRepository.findAll().size());
+    }
+
+    @Test
+    void testDeletePerson() throws Exception {
+        var person = new Person();
+        person.setFirstName("Max");
+        person.setLastName("Mustermann");
+        person.setBirthday(LocalDate.now());
+
+        person = personRepository.save(person);
+
+        var response = this.mvc.perform(
+                delete("/persons/" + person.getId())
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatus());
+        assertTrue(personRepository.findAll().isEmpty());
+    }
+
+    // TODO: Add more test cases here
+}

src/test/java/de/tum/in/ase/eist/PersonServiceTest.java
@@ -1,0 +1,55 @@
+package de.tum.in.ase.eist;
+
+import de.tum.in.ase.eist.model.Person;
+import de.tum.in.ase.eist.repository.PersonRepository;
+import de.tum.in.ase.eist.service.PersonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+class PersonServiceTest {
+    @Autowired
+    private PersonService personService;
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void testAddPerson() {
+        var person = new Person();
+        person.setFirstName("Max");
+        person.setLastName("Mustermann");
+        person.setBirthday(LocalDate.now());
+
+        personService.save(person);
+
+        assertEquals(1, personRepository.findAll().size());
+    }
+
+    @Test
+    void testDeletePerson() {
+        var person = new Person();
+        person.setFirstName("Max");
+        person.setLastName("Mustermann");
+        person.setBirthday(LocalDate.now());
+
+        person = personRepository.save(person);
+
+        personService.delete(person);
+
+        assertTrue(personRepository.findAll().isEmpty());
+    }
+
+    // TODO: Add more test cases here
+}